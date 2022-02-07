eb07680570c8f1b5e7c21c625cfe4a85370e72c4
cheiray <84770232+cheiray@users.noreply.github.com>
2022-02-07 16:25:06 +0100

    Pick Object Script Added
    Pick Object Script Added

---
+++ Unity/ProjetOS09B/Assets/Scripts/PickObject.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject player;
+    void Start()
+    {
+        player = GameObject.Find("Your_Name_Here");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void pickup()
+    {
+        //recupérer la variable globale du nombre de copies de l'utilisateur et l'incrémenter d'un
+    }
+
+
+    void OnMouseDown()
+    {
+        /*float minDist = 2;
+        float dist = Vector3.Distance(other.position, player.transform.position);
+        if (dist < minDist)
+        {
+            pickup();
+        }*/
+    }
+}
