52699cd7c40be0bcc48fd1a50091e7c220ae7de6
cheiray <84770232+cheiray@users.noreply.github.com>
2022-02-07 17:41:36 +0100

    Companion

---
+++ Unity/ProjetOS09B/Assets/Scripts/Companion.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    /*NavMeshAgent myNavMeshAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myNavMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.X))
+        {
+            //bait the teacher
+            SetDestinationToMousePosition();
+        }
+    }
+
+    void SetDestinationToMousePosition()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            myNavMeshAgent.SetDestination(hit.point);
+        }
+    }*/
+}
