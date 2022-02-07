1b698e71856abc5027972a827be601811f2d36a2
Luc DELVALLE <luc.delvalle@etu.univ-savoie.fr>
2022-02-07 17:41:32 +0100

    ds

--- Unity/ProjetOS09B/Assets/Scripts/LineOfSight.cs
+++ Unity/ProjetOS09B/Assets/Scripts/LineOfSight.cs
@@ -22,6 +22,9 @@ public class LineOfSight : MonoBehaviour
                 if (hit.transform == player.transform && Vector3.Angle(rayDirection, transform.forward) < 60) {
                     print("Player seen");
                 }
+                if (hit.transform == player.transform && player.isCrouching == true && rayDirection.magnitude < 1.0f) {
+                    print("Player crouched heard");
+                }
                 if (hit.transform == player.transform && rayDirection.magnitude < 4.0f) {
                     print("Player heard");
                 }
