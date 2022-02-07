246eef31a3c8b0d15c1e6bf4e91d23beb0425c8a
Luc DELVALLE <luc.delvalle@etu.univ-savoie.fr>
2022-02-07 17:01:03 +0100

    Update LineOfSight.cs

--- Unity/ProjetOS09B/Assets/Scripts/LineOfSight.cs
+++ Unity/ProjetOS09B/Assets/Scripts/LineOfSight.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class LineOfSight : MonoBehaviour
 {
+    public GameObject player;
+    private RaycastHit hit;
+    private Vector3 rayDirection;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rayDirection = player.transform.position - transform.position;
+        if (Physics.Raycast(transform.position, rayDirection, out hit, 10.0f))
+                if (hit.transform == player.transform && Vector3.Angle(rayDirection, transform.forward) < 60) {
+                    print("Player seen");
+                }
+                if (hit.transform == player.transform && rayDirection.magnitude < 4.0f) {
+                    print("Player heard");
+                }
     }
 }
