70b139566a3e8938c95e66219b4ad3300ae56fda
Hatu7 <77270670+FireStorm753@users.noreply.github.com>
2022-02-23 13:03:12 +0100

    input

--- Unity/ProjetOS09B/Assets/Scripts/LineOfSight.cs
+++ Unity/ProjetOS09B/Assets/Scripts/LineOfSight.cs
@@ -7,11 +7,13 @@ public class LineOfSight : MonoBehaviour
     public GameObject player;
     private RaycastHit hit;
     private Vector3 rayDirection;
+    private PlayerMovement playerMovement;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindWithTag("Player");
+        playerMovement = player.GetComponent<PlayerMovement>();
     }
 
     // Update is called once per frame
@@ -22,10 +24,10 @@ public class LineOfSight : MonoBehaviour
                 if (hit.transform == player.transform && Vector3.Angle(rayDirection, transform.forward) < 60) {
                     print("Player seen");
                 }
-                if (hit.transform == player.transform && player.isCrouching == true && rayDirection.magnitude < 1.0f) {
+                else if (playerMovement.isMoving && playerMovement.isCrouching && rayDirection.magnitude < 1.0f) {
                     print("Player crouched heard");
                 }
-                if (hit.transform == player.transform && rayDirection.magnitude < 4.0f) {
+                else if (playerMovement.isMoving && rayDirection.magnitude < 3.0f) {
                     print("Player heard");
                 }
     }
--- Unity/ProjetOS09B/Assets/Scripts/playerMovement.cs
+++ Unity/ProjetOS09B/Assets/Scripts/playerMovement.cs
@@ -2,21 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    private bool isMoving = false;
-    private bool isSprinting = false;
+    private CharacterController controller;
+
+    public bool isMoving = false;
+    public bool isSprinting = false;
     public bool isCrouching = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = gameObject.AddComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         if (Input.GetKeyDown(KeyCode.LeftControl))
         {
             isCrouching = true;
