097d3ad1293abc2a0effd98fa6bca4d0daee4df5
Hatu7 <77270670+FireStorm753@users.noreply.github.com>
2022-03-14 11:59:10 +0100

    inputs

--- Unity/ProjetOS09B/Assets/Scripts/Player/Inputs/Interact.cs
+++ Unity/ProjetOS09B/Assets/Scripts/Player/Inputs/Interact.cs
@@ -10,22 +10,25 @@ public class Interact : MonoBehaviour
 
     public void OnInteract (InputAction.CallbackContext context)
     {
-        if (context.started)
+        if (context.performed)
             interact = true;
-        else if (context.performed)
-            interact = false;
+        // else if (context.performed)
+        //     interact = false;
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        print("a");
     }
 
     // Update is called once per frame
     void Update()
     {
+        print("a");
         if (Physics.Raycast(transform.position, transform.forward, out hit, 3.0f))
         {
+            
             if (hit.transform.tag == "Interactable" && interact == true) {
                 print("Interacted with");   
                    
