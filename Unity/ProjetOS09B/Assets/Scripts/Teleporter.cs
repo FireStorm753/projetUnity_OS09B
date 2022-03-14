00bd574b1d3fd0a79631f41d57e4b348272beaee
Nagatsukii <74364251+Nagatsukii@users.noreply.github.com>
2022-03-14 17:18:41 +0100

    Téléportation entre les 2 étages

---
+++ Unity/ProjetOS09B/Assets/Scripts/Teleporter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject Teleporter1To;
+    public GameObject Teleporter2To;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Teleporter1"))
+        {
+            Player.transform.position = Teleporter1To.transform.position;
+        }
+
+        if (collision.gameObject.CompareTag("Teleporter2"))
+        {
+            Player.transform.position = Teleporter2To.transform.position;
+        }
+    }
+}
