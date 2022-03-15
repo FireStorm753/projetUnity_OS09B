5c66f8bfd7a0a7922b798d1d5b01bcaeba2e839f
cheiray <84770232+cheiray@users.noreply.github.com>
2022-03-15 14:10:49 +0100

    Game Over

---
+++ Unity/ProjetOS09B/Assets/Scripts/GameOver.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+using System.Linq;
+using System.Collections.Generic;
+
+public class GameOver : MonoBehaviour
+{
+    public static List<int> RealPageList = new List<int>();
+
+    public void RestartGame()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            int random = (int) UnityEngine.Random.Range(0, 10)+1;
+            if (RealPageList.Contains(random))
+                i--;
+            else
+                RealPageList.Add(random);
+        }
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void MenuGame()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
