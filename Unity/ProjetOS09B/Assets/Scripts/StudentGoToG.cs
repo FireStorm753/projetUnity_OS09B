0fc2cf80acf5175221f437278b424c0b465a2d25
Nagatsukii <74364251+Nagatsukii@users.noreply.github.com>
2022-03-15 22:40:43 +0100

    music

---
+++ Unity/ProjetOS09B/Assets/Scripts/StudentGoToG.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StudentGoToG : MonoBehaviour
+{
+    UnityEngine.AI.NavMeshAgent student;
+    float distanceFromCamera = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            Vector3 centerPos = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, distanceFromCamera));
+            student.GetComponent<UnityEngine.AI.NavMeshAgent>().Warp(centerPos);
+        }
+    }
+}
---
+++ Unity/ProjetOS09B/Assets/Scripts/music2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class music2 : MonoBehaviour
+{
+    public GameObject prof;
+    AudioSource audios;
+    // Start is called before the first frame update
+    void Start()
+    {
+        prof = GameObject.FindWithTag("Prof");
+        audios = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool chase2 = PatrollingIA.chasing;
+        if (chase2 && audios.isPlaying == false)
+            audios.Play();
+        else if (!chase2 && audios.isPlaying == true)
+            audios.Stop();
+    }
+}
