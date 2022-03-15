c970baae2625d9d1673d0e010bf69b6c71d3e63b
Nagatsukii <74364251+Nagatsukii@users.noreply.github.com>
2022-03-15 09:37:30 +0100

    Student

--- Unity/ProjetOS09B/Assets/Scripts/Student.cs
+++ Unity/ProjetOS09B/Assets/Scripts/Student.cs
@@ -4,17 +4,32 @@ using UnityEngine;
 
 public class Student : MonoBehaviour
 {
-    public Animator ani;
+    public GameObject player;
+    private UnityEngine.AI.NavMeshAgent student;
+    Animator ani;
 
     // Start is called before the first frame update
     void Start()
     {
+        student = GetComponent<UnityEngine.AI.NavMeshAgent>();
         ani = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        bool isMoving = GameObject.FindWithTag("Player").GetComponent<PlayerMovement>().isMoving;
+        if (isMoving == true) 
+        {
+            Debug.Log("Bouge");
+            ani.SetBool("Run", true);
+            student.SetDestination(player.transform.position);
+        }
+        else
+        {
+            Debug.Log("Bouge pas");
+            ani.SetBool("Run", false);
+            student.SetDestination(student.transform.position);
+        }
     }
 }
