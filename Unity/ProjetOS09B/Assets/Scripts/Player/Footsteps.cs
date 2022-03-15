05e086bd3bb498154bcf546756d383611282cde1
Nagatsukii <74364251+Nagatsukii@users.noreply.github.com>
2022-03-15 20:05:53 +0100

    Sounds

---
+++ Unity/ProjetOS09B/Assets/Scripts/Player/Footsteps.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour
+{
+    public GameObject player;
+    AudioSource audios;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        audios = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isMoving = GameObject.FindWithTag("Player").GetComponent<PlayerMovement>().isMoving;
+        if (isMoving && audios.isPlaying == false)
+            audios.Play();
+        else if (!isMoving && audios.isPlaying == true)
+            audios.Stop();
+    }
+}
---
+++ Unity/ProjetOS09B/Assets/Scripts/music.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class music : MonoBehaviour
+{
+    public GameObject prof;
+    AudioSource audios;
+    // Start is called before the first frame update
+    void Start()
+    {
+        prof = GameObject.FindWithTag("Prof");
+        audios = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool chase = Teacher.chasing;
+        if (chase && audios.isPlaying == false)
+            audios.Play();
+        else if (!chase && audios.isPlaying == true)
+            audios.Stop();
+    }
+}
